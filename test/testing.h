409526a212ffce27bceecef7daeee1ffe935fd2e
Benny Baumann <baumann@4h-jena.de>
2021-08-07 18:16:27 +0200

    Introduce some more color to the tests

--- test/testing.h
+++ test/testing.h
@@ -7,12 +7,12 @@
 
 #define TEST(EXPR, EXPECTED) \
     do { \
-        printf("Testing %s ... ", #EXPR); \
+        printf("Testing \e[1m%s\e[0m ... ", #EXPR); \
         const int r = EXPR, v = EXPECTED; \
         if(r == v) { \
-            printf("OK\n"); \
+            printf("\e[1;32mOK\e[0m\n"); \
         } else { \
-            printf("FAIL: %s != %s (%d != %d)\n", #EXPR, #EXPECTED, r, v); \
+            printf("\e[1;31mFAIL\e[0m: %s != %s \e[1m(\e[31m%d\e[0;1m != \e[32m%d\e[0;1m)\e[0m\n", #EXPR, #EXPECTED, r, v); \
             result = false; \
         }; \
     } while(0)
